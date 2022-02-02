1c70c6477493adf16ef2084b5876311b2f9eb68f
carlynhanson <85962504+carlynhanson@users.noreply.github.com>
2022-02-02 14:33:27 -0600

    Add files via upload
    Added js folder and html index

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(221, 48, 78) // 0-255 color scheme
+
+
+}
+
+function draw() {
+  fill(10, 45, 89) //color fill before shape
+  ellipse(300, 300, 50, 50) //first two are x, y coordinates
+
+}
